62686b5ad69c5962628c08b6ba48bf6e0cec7392
CajjadCodes <62202221+CajjadCodes@users.noreply.github.com>
2020-07-31 20:30:08 +0430

    Debug OpcDcd

--- OpcDcd.v
+++ OpcDcd.v
@@ -1,7 +1,7 @@
 module OpcDcd(opc,inst,RT,addi,andi,lw,sw,j,beq,bne,nop);
 input [5:0]opc;
 input [31:0]inst;
-output reg RT;
+output RT;
 output reg addi;
 output reg andi;
 output reg lw;
@@ -11,9 +11,8 @@ output reg beq;
 output reg bne;
 output nop;
 	always@(opc)begin
-		{RT,addi,andi,lw,sw,j,beq,bne}=8'b0;
+		{addi,andi,lw,sw,j,beq,bne}=8'b0;
 		case(opc)
-			6'b000000: RT=1'b1;
 			6'b001000: addi=1'b1;
 			6'b001100: andi=1'b1;
 			6'b100011: lw=1'b1;
@@ -23,5 +22,8 @@ output nop;
 			6'b000101: bne=1'b1;
 		endcase
 	end
+
 	assign nop = ~(|inst);
+	assign RT = (~nop) & (opc == 6'b000000);
+
 endmodule
