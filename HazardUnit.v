472f792bddab14a90b954171c9ab91d33ce41fa0
MehdiSaraie <62212279+MehdiSaraie@users.noreply.github.com>
2020-08-01 18:42:54 +0430

    naming

--- HazardUnit.v
+++ HazardUnit.v
@@ -1,12 +1,12 @@
-module HazardUnit(Rs,Rt,ID_EX_MemRead,ID_EX_Rt,ChSel,PCWrite,IF_ID_Write);
-input [4:0]Rs;
-input [4:0]Rt;
+module HazardUnit(ID_Rs,ID_Rt,ID_EX_MemRead,ID_EX_Rt,CSSel,PCWrite,IF_ID_Write);
+input [4:0]ID_Rs;
+input [4:0]ID_Rt;
 input ID_EX_MemRead;
 input [4:0]ID_EX_Rt;
-output ChSel;
+output CSSel;
 output PCWrite;
 output IF_ID_Write;
-	assign ChSel = ~(ID_EX_MemRead & ((ID_EX_Rt == Rs) | (ID_EX_Rt == Rt)) & ID_EX_Rt!=5'b0);
-	assign PCWrite = ChSel;
-	assign IF_ID_Write = ChSel;
+	assign CSSel = ~(ID_EX_MemRead & ((ID_EX_Rt == ID_Rs) | (ID_EX_Rt == ID_Rt)) & ID_EX_Rt!=5'b0);
+	assign PCWrite = CSSel;
+	assign IF_ID_Write = CSSel;
 endmodule
