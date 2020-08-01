69c10623b2fa9afe0b75df433077d3d76101dd14
MehdiSaraie <62212279+MehdiSaraie@users.noreply.github.com>
2020-08-01 22:42:41 +0430

    Update OpcDcd.v

--- OpcDcd.v
+++ OpcDcd.v
@@ -1,5 +1,5 @@
-module OpcDcd(opc,inst,RT,addi,andi,lw,sw,j,beq,bne,nop);
-input [5:0]opc;
+module OpcDcd(Opc,inst,RT,addi,andi,lw,sw,j,beq,bne,nop);
+input [5:0]Opc;
 input [31:0]inst;
 output RT;
 output reg addi;
@@ -10,9 +10,9 @@ output reg j;
 output reg beq;
 output reg bne;
 output nop;
-	always@(opc)begin
+	always@(Opc)begin
 		{addi,andi,lw,sw,j,beq,bne}=8'b0;
-		case(opc)
+		case(Opc)
 			6'b001000: addi=1'b1;
 			6'b001100: andi=1'b1;
 			6'b100011: lw=1'b1;
@@ -24,6 +24,6 @@ output nop;
 	end
 
 	assign nop = ~(|inst);
-	assign RT = (~nop) & (opc == 6'b000000);
+	assign RT = (~nop) & (Opc == 6'b000000);
 
 endmodule
